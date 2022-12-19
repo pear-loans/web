d7b1d8d802a1a5dc3f67a1cdca300d8c9c502dfa
Austin Meadows <austin@slowp.ke>
2022-12-19 10:31:24 -0500

    🔧 remove App declaration

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,28 @@
 /* @refresh reload */
 import { MetaProvider, Title } from "@solidjs/meta";
-import type { JSX } from "solid-js";
 import { render } from "solid-js/web";
 import { Toaster } from "solid-toast";
 
 import icons from "🍐/config/icons";
 import Router from "🍐/router";
 
-const App = (): JSX.Element => (
-  <MetaProvider>
-    <Title>Pear Loans</Title>
-    <Toaster
-      gutter={10}
-      position="top-center"
-      toastOptions={{
-        ariaProps: {
-          "aria-live": "polite",
-          role: "status",
-        },
-        icon: icons.info,
-      }}
-    />
-    <Router />
-  </MetaProvider>
+render(
+  () => (
+    <MetaProvider>
+      <Title>Pear Loans</Title>
+      <Toaster
+        gutter={10}
+        position="top-center"
+        toastOptions={{
+          ariaProps: {
+            "aria-live": "polite",
+            role: "status",
+          },
+          icon: icons.info,
+        }}
+      />
+      <Router />
+    </MetaProvider>
+  ),
+  document.body
 );
-
-render(() => <App />, document.body);
-
-export default App;
