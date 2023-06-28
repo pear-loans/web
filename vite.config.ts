6f329f71456bbd421f79a22bbc952a0442946c13
Austin Meadows <slowpoke@slowp.ke>
2023-06-28 15:16:25 -0400

    📦 package update

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,30 @@
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
-import postcss_lightningcss from "postcss-lightningcss";
+
 import postcss_variables from "postcss-variable-compress";
 import postcss_tailwind from "tailwindcss";
-import tsconfigPaths from "vite-tsconfig-paths";
+
+import vite_lightningcss from "vite-plugin-lightningcss";
+import vite_paths from "vite-tsconfig-paths";
 
 import type { UserConfig } from "vite";
 
 import { browserslist } from "./package.json";
 
 export default {
-	plugins: [tsconfigPaths(), qwikCity(), qwikVite()],
+	plugins: [
+		vite_paths(),
+		qwikCity(),
+		qwikVite(),
+		vite_lightningcss({ browserslist }),
+	],
 	css: {
 		postcss: {
-			plugins: [
-				postcss_tailwind(),
-				postcss_variables(),
-				postcss_lightningcss({ browsers: browserslist }),
-			],
+			plugins: [postcss_tailwind(), postcss_variables()],
 		},
+	},
+	optimizeDeps: {
+		include: ["@auth/core"],
 	},
 	preview: {
 		headers: {
