d3fbf5933fb406a552b49d65915b6fe7336a3914
Austin Meadows <slowpoke@slowp.ke>
2023-07-06 11:03:42 -0400

    🏗️ more build fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 
-import postcss_lightningcss from "postcss-lightningcss";
-import postcss_variablecompress from "postcss-variable-compress";
-import postcss_tailwind from "tailwindcss";
-
 import vite_paths from "vite-tsconfig-paths";
 
 import { defineConfig } from "vite";
-
-import { browserslist } from "./package.json";
 
 export default defineConfig(() => ({
 	plugins: [qwikCity(), qwikVite(), vite_paths()],
