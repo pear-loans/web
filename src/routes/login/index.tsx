837a05a5c74c5873f3df1391dcc571eb13be748e
Austin Meadows <slowpoke@slowp.ke>
2023-07-12 19:00:24 -0400

    🙃 console log

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -3,9 +3,8 @@ import { component$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import { type D1Database } from "@cloudflare/workers-types";
 
-export const useGetDb = routeLoader$(({ platform }) => {
-	const { DB_USERS } = platform as { DB_USERS: D1Database };
-	console.log(DB_USERS);
+export const useGetDb = routeLoader$((thing) => {
+	console.log("fuck you", thing);
 });
 
 export default component$(() => {
