1bfddbc089aa9e97da40c64b6354eaec0fa076b9
Austin Meadows <slowpoke@slowp.ke>
2023-07-13 10:18:53 -0400

    🧼 more cleanup

--- config/d1-setup.sql
+++ config/d1-setup.sql
@@ -1,3 +1,5 @@
+-- This file clears and sets up a new database with fake data.
+
 DROP TABLE IF EXISTS Users;
 CREATE TABLE IF NOT EXISTS Users (Id TEXT PRIMARY KEY, Platform TEXT, Email TEXT);
 INSERT INTO Users (Id, Platform, Email) VALUES ('1', 'Google', 'test1@test.com'), ('4', 'Apple', 'test2@test.com');
