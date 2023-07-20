e1e594315eee695928f085f29d564aa2b55d7cfa
Austin Meadows <slowpoke@slowp.ke>
2023-07-20 18:10:12 -0400

    ✔ no-check for testing

--- src/components/routes/account/profile.tsx
+++ src/components/routes/account/profile.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { component$ } from "@builder.io/qwik";
 import { Form } from "@builder.io/qwik-city";
 
--- src/routes/account/index.tsx
+++ src/routes/account/index.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { component$ } from "@builder.io/qwik";
 import { routeAction$, routeLoader$ } from "@builder.io/qwik-city";
 
