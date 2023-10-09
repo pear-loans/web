06dac18d19594d512c47a9ec996df8524124c43f
Austin Meadows <slowpoke@slowp.ke>
2023-10-09 15:32:37 -0400

    🧼 header spacing fixes

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,7 +1,7 @@
 import { Slot, component$ } from "@builder.io/qwik";
 
 export default component$(() => (
-	<main class="mx-auto max-w-screen-2xl px-1 md:px-5">
+	<main class="mx-auto max-w-screen-2xl px-3 md:px-5">
 		<Slot />
 	</main>
 ));
