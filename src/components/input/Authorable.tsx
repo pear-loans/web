bddf64d9a6fdfd0df935433dcd2da999c720cd4b
Austin Meadows <slowpoke@slowp.ke>
2023-10-10 20:11:33 -0400

    🔧 fix colors

--- src/components/input/Authorable.tsx
+++ src/components/input/Authorable.tsx
@@ -28,7 +28,7 @@ export default component$<Props>(({ class: className = undefined, label = undefi
 		>
 			<div
 				class={[
-					"h-50 w-full flex animate-pulse items-center justify-center bg-black/10",
+					"h-50 w-full flex animate-pulse items-center justify-center bg-black/10 dark:bg-gray-700/10",
 					borderClasses
 				]}
 			>
@@ -44,7 +44,7 @@ export default component$<Props>(({ class: className = undefined, label = undefi
 					title: "Edit content"
 				}}
 				class={[
-					"absolute right-0 top-0 h-full border-l-0 rounded-l-0 bg-green-200/50 px-4 text-black",
+					"absolute right-0 top-0 h-full border-l-0 rounded-l-0 bg-green-100/50 px-4 text-black dark:text-white dark:bg-black/50",
 					borderClasses
 				]}
 				icon={faPenToSquare}
